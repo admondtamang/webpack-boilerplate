9c312ee0fc58120d3a5976647b2be6ffec7c4819
Admond Tamang <admondtamang@gmail.com>
2021-03-03 16:31:28 +0545

    [fix] error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
- <p>hello</p>,   document.getElementById("root")
+ReactDOM.render(<p>hello</p>, document.getElementById("root"));
