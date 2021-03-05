9909289401e03f13f2bc7a0b02986cfc26e2f349
Admond Tamang <admondtamang@gmail.com>
2021-03-05 13:29:53 +0545

    [add] production,dev and common webpack config

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<p>hello</p>, document.getElementById("root"));
---
+++ webpack.common.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: ["@babel/polyfill", "./src/index.js"],
+    // mode: "development",
+    output: {
+        // `filename` provides a template for naming your bundles (remember to use `[name]`)
+        filename: "[name].bundle.js",
+        // `chunkFilename` provides a template for naming code-split bundles (optional)
+        chunkFilename: "[name].bundle.js",
+        path: path.resolve(__dirname, "./dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                // Apply rule for .sass, .scss or .css files
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|eot|ttf|svg|gif|woff|jpg)$/,
+                use: ["url-loader"],
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                common: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "common",
+                    chunks: "all",
+                },
+            },
+        },
+    },
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: "disabled",
+        }),
+        new HtmlWebpackPlugin({
+            template: "template.html",
+        }),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+        }),
+    ],
+};
