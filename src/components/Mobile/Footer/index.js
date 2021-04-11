ffa6436f0323baf124921f166bffaa40a035c661
Admond Tamang <admondtamang@gmail.com>
2021-04-11 11:11:17 +0545

    [add] projects

--- src/components/Mobile/Footer/index.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-export default function Footer() {
-    // home, category, cart, login
-    const [selectedTab, setSelectedTab] = useState("home");
-    return <p>footer</p>;
-}
--- src/pages/Brands/BrandsDetail/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function BrandsDetail() {
-    return <div className="brandDetail"></div>;
-}
